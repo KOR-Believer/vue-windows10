6be3af13690ac30b78d2aff73b959725d7e55526
kiseong.park <kiseong.park@stunitas.com>
2019-08-28 15:54:38 +0900

    [build] production build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/' // ? '/production-sub-path/'
+    ? '/vue-windows10/'
     : '/',
-    outputDir: 'docs'
+  outputDir: 'docs'
 }
